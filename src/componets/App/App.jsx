246e041b8c1db51ba8b5406f13ffef6c2af82402
limawkins <102392421+limawkins@users.noreply.github.com>
2023-05-21 11:58:22 +0300

    Инициализация проекта

--- src/componets/App/App.jsx
+++ src/componets/App/App.jsx
@@ -1,21 +1,11 @@
 import "./App.css";
 import Card from "../Card/Card.jsx";
 import { useState } from "react";
-
 const App = () => {
   const [result, setResult] = useState(3);
-  const handleClick = (operator) => {
-    if (operator === "plus") {
-      setResult(result + 1);
-    } else if (operator === "minus") {
-      setResult(result - 1);
-    }
-  };
+
   return (
     <article>
-      <button onClick={() => handleClick("minus")}>-1</button>
-      <p>{result}</p>
-      <button onClick={() => handleClick("plus")}>+1</button>
       <Card />
     </article>
   );
--- src/componets/Card/Card.jsx
+++ src/componets/Card/Card.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./Card.css";
+import { ReactComponent as PauseIcon } from "../../assets/pause.svg";
+import { ReactComponent as DiceIcon } from "../../assets/dice.svg";
+import classNames from "classnames";
 const Card = () => {
   // https://api.adviceslip.com/advice
   const [isLoading, setIsLoading] = useState(false);
-  const [advice, setAdvice] = useState();
+  const [advice, setAdvice] = useState({ id: 1, body: 1 });
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://api.adviceslip.com/advice")
-      .then((response) => response.json())
-      .then((data) =>
-        setAdvice({
-          id: data.slip.id,
-          body: data.slip.advice,
-        })
-      )
-      .finally(() => setIsLoading(false));
+    fetchAdvice();
   }, []);
-  const handleClick = () => {
+  const fetchAdvice = () => {
     setIsLoading(true);
     fetch("https://api.adviceslip.com/advice")
       .then((response) => response.json())
@@ -30,14 +24,26 @@ const Card = () => {
   };
   return (
     <article>
-      <h1>ADVICE #{advice.id}</h1>
+      <h1>#{advice.id}</h1>
       <blockquote>{advice.body}</blockquote>
+      <div className="decoration">
+        <span className="line"></span>
+        <div className="icon">
+          <PauseIcon />
+        </div>
+        <span className="line"></span>
+      </div>
       <button
         disabled={isLoading}
-        className={isLoading ? "loading" : ""}
-        onClick={handleClick}
+        className={classNames(
+          {
+            loading: isLoading,
+          },
+          "button"
+        )}
+        onClick={fetchAdvice}
       >
-        Кнопка
+        <DiceIcon />
       </button>
     </article>
   );
